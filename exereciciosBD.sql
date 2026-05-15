c5c4fcfec386c2f685fc137a9b71863a82e338a4
Richy-2003 <rchadryan@gmail.com>
2026-05-15 20:25:15 -0300

    Update exereciciosBD.sql

--- exereciciosBD.sql
+++ exereciciosBD.sql
@@ -31,6 +31,24 @@ IdFuncionario INT,
 IdProjetos INT,
 DataEntrada DATE)
 
+INSERT INTO Departamentos (Id, Nome, DataCriacao)
+VALUES 
+	(TI, 01/01/2026),
+	(RH,15/03/2021),
+	(Financeiro, 10/07/2026);
+
+INSERT INTO Funcionarios (Id, Nome, Email, Salario, DataAdimissao, IdDepartamento)
+VALUES 
+	(Carlos Silva, carlos@empresa.com, 5000,00, 10/05/2022, TI),
+	(Ana Souza, ana@empresa.com, 7000,00, 20/08/2021, RH),
+	(Pedro Lima, pedro@empresa.com, 4500,00, Financeiro);
+
+INSERT INTO Projetos (Nome, Orcamento, DataInicio, DataFim)
+VALUES 
+	(Sistemas ERP, 100,000,00, 01/10/2023, 31/12/2023),
+	(Recrutamento Digital, 50,000,00, 01/06/2023, 01/10/2023),
+	(Controle Financeiro, 75,000,00, 01/03/2023, 30/09/2023);
+
 
 
 
