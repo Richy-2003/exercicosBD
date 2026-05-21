f8e7f5d910eafa61e85de974ef85dda5d69ea2c8
Richy-2003 <rchadryan@gmail.com>
2026-05-21 19:52:57 -0300

    Update exereciciosBD.sql

--- exereciciosBD.sql
+++ exereciciosBD.sql
@@ -3,34 +3,35 @@ CREATE DATABASE EmpresaDB;
 USE EmpresaDB;
 
 CREATE TABLE Departamentos (
-    Id INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(100) NOT NULL UNIQUE,
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Nome VARCHAR(200) UNIQUE NOT NULL,
     DataCriacao DATE
 );
 
 CREATE TABLE Funcionarios (
-    Id INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(150) NOT NULL,
-    Email VARCHAR(150) UNIQUE,
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Nome VARCHAR(200) NOT NULL,
+    Email VARCHAR(200) UNIQUE,
     Salario DECIMAL(10,2),
     DataAdmissao DATE,
     IdDepartamento INT NOT NULL,
 
-    CONSTRAINT FK_Funcionario_Departamento
+    CONSTRAINT FK_Func_Dep
     FOREIGN KEY (IdDepartamento)
     REFERENCES Departamentos(Id)
 );
 
 CREATE TABLE Projetos (
-    Id INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(150) NOT NULL,
-    Orcamento DECIMAL(12,2),
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Nome VARCHAR(200) NOT NULL,
+    Orcamento DECIMAL(10,2),
     DataInicio DATE,
     DataFim DATE
 );
 
 CREATE TABLE FuncionariosProjetos (
-    IdFuncionarioProjeto INT PRIMARY KEY AUTO_INCREMENT,
+    IdFuncionarioProjeto INT PRIMARY KEY IDENTITY(1,1),
+
     IdFuncionario INT NOT NULL,
     IdProjeto INT NOT NULL,
     DataEntrada DATE,
@@ -44,8 +45,7 @@ CREATE TABLE FuncionariosProjetos (
     REFERENCES Projetos(Id)
 );
 
-INSERT INTO Departamentos
-(Nome, DataCriacao)
+INSERT INTO Departamentos (Nome, DataCriacao)
 VALUES
 ('TI','2020-01-01'),
 ('RH','2021-03-15'),
@@ -54,109 +54,66 @@ VALUES
 INSERT INTO Funcionarios
 (Nome, Email, Salario, DataAdmissao, IdDepartamento)
 VALUES
-(
-'Carlos Silva',
+
+('Carlos Silva',
 'carlos@empresa.com',
 5000.00,
 '2022-05-10',
-(SELECT Id FROM Departamentos WHERE Nome='TI')
-),
+1),
 
-(
-'Ana Souza',
+('Ana Souza',
 'ana@empresa.com',
 7000.00,
 '2021-08-20',
-(SELECT Id FROM Departamentos WHERE Nome='RH')
-),
+2),
 
-(
-'Pedro Lima',
+('Pedro Lima',
 'pedro@empresa.com',
 4500.00,
 '2023-01-15',
-(SELECT Id FROM Departamentos WHERE Nome='Financeiro')
-);
+3);
 
 INSERT INTO Projetos
 (Nome, Orcamento, DataInicio, DataFim)
 VALUES
-(
-'Sistema ERP',
+
+('Sistema ERP',
 100000.00,
 '2023-01-01',
-'2023-12-31'
-),
+'2023-12-31'),
 
-(
-'Recrutamento Digital',
+('Recrutamento Digital',
 50000.00,
 '2023-06-01',
-'2023-10-01'
-),
+'2023-10-01'),
 
-(
-'Controle Financeiro',
+('Controle Financeiro',
 75000.00,
 '2023-03-01',
-'2023-09-30'
-);
+'2023-09-30');
 
 INSERT INTO FuncionariosProjetos
 (IdFuncionario, IdProjeto, DataEntrada)
 VALUES
 
-(
-(SELECT Id FROM Funcionarios
-WHERE Nome='Carlos Silva'),
+(1,1,GETDATE()),
+(1,3,GETDATE()),
+(2,2,GETDATE()),
+(3,1,GETDATE()),
+(3,3,GETDATE());
 
-(SELECT Id FROM Projetos
-WHERE Nome='Sistema ERP'),
+SELECT
+f.Nome AS Funcionario,
+d.Nome AS Departamento,
+p.Nome AS Projeto
 
-CURRENT_DATE
-),
+FROM FuncionariosProjetos fp
 
-(
-(SELECT Id FROM Funcionarios
-WHERE Nome='Carlos Silva'),
+INNER JOIN Funcionarios f
+ON fp.IdFuncionario = f.Id
 
-(SELECT Id FROM Projetos
-WHERE Nome='Controle Financeiro'),
+INNER JOIN Departamentos d
+ON f.IdDepartamento = d.Id
 
-CURRENT_DATE
-),
-
-(
-(SELECT Id FROM Funcionarios
-WHERE Nome='Ana Souza'),
-
-(SELECT Id FROM Projetos
-WHERE Nome='Recrutamento Digital'),
-
-CURRENT_DATE
-),
-
-(
-(SELECT Id FROM Funcionarios
-WHERE Nome='Pedro Lima'),
-
-(SELECT Id FROM Projetos
-WHERE Nome='Sistema ERP'),
-
-CURRENT_DATE
-),
-
-(
-(SELECT Id FROM Funcionarios
-WHERE Nome='Pedro Lima'),
-
-(SELECT Id FROM Projetos
-WHERE Nome='Controle Financeiro'),
-
-CURRENT_DATE
-);
-
-
-
-
-
+INNER JOIN Projetos p
+ON fp.IdProjeto = p.Id;
